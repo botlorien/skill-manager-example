f198a9cf8ac3fbd85b302fb481881f46edf869c5
BEN-HUR PEREIRA BORGES DOS SANTOS <138973267+botlorien@users.noreply.github.com>
2024-10-31 17:54:15 -0400

    Add files via upload

---
+++ skill_manager.py
@@ -0,0 +1,61 @@
+
+class SkillManager:
+
+    def __init__(self) -> None:
+        self._candidato_habilidades:dict = {}
+        self._requisitos_cargo:dict = {}
+
+    def __repr__(self) -> str:
+        return f'SkillManager(cadidato_habilidades={self._candidato_habilidades}, requisitos_cargo={self._requisitos_cargo})'
+
+    def adicionar_candidato(self,nome:str, habilidades:set):
+        if isinstance(nome,str) and isinstance(habilidades,set):
+            self._candidato_habilidades[nome] = habilidades
+        else:
+            raise TypeError(
+                f"""Invalid type for nome or habilidades parameters: nome: {nome}, habilidades: {habilidades}. Expected str(nome) and set(habilidades)"""
+            )
+
+    def adicionar_cargo(self,nome:str, requisitos:set):
+        if isinstance(nome,str) and isinstance(requisitos,set):
+            self._requisitos_cargo[nome] = requisitos
+        else:
+            raise TypeError(
+                f"""Invalid type for nome or habilidades parameters: nome: {nome}, requisitos: {requisitos}. Expected str(nome) and set(requisitos)"""
+            )
+
+    def verificar_compatibilidade(self,candidato:str,cargo:str):
+        return {"habilidades_em_comum": self._candidato_habilidades[candidato] & self._requisitos_cargo[cargo],
+                "habilidades_faltantes": self._requisitos_cargo[cargo] - self._candidato_habilidades[candidato]}
+
+    def listar_todos_candidatos(self):
+        return self._candidato_habilidades
+
+    def listar_todos_cargos(self):
+        return self._requisitos_cargo
+    
+if __name__=='__main__':
+    # Criando uma instancia
+    sk = SkillManager()
+
+    # Adicionando um candidato
+    sk.adicionar_candidato(
+        'João',{'python','javascript'}
+    )
+    
+    # Adicionando um cargo
+    sk.adicionar_cargo(
+        'Desenvolvedor',{'python','sql','docker'}
+    )
+
+    # Listando os candidatos e cargos
+    print(f"Cargos: {sk.listar_todos_cargos()}")
+    print(f'Candidatos: {sk.listar_todos_candidatos()}')
+
+    # Verificando compatibilidade
+    print("Compatibilidades: ",
+        sk.verificar_compatibilidade(
+            'João','Desenvolvedor'
+        )
+    )
+    
